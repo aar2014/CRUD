e25a3811825ad9c987a1640eb3d2c3ca65dfd3d0
vaishali shah <vaishalidotshah@gmail.com>
2018-02-08 12:22:13 -0800

    # WARNING: head commit changed in the meantime
    added new fields

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , inventory = require('./routes/inventory')
   , http = require('http')
   , path = require('path');
 
@@ -27,21 +28,31 @@ if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
 
-//app.get('/', routes.index);
-
-app.get('/',function(req,res){
-    res.render('index',{
-        title : 'Inventory Form',
-    });
-    console.log('user accessing Home page');
+app.use(function(req,res,next){
+    res.locals.userValue = null;
+    next();
 });
 
+app.get('/', routes.index);
+
+//app.get('/',function(req,res){
+//    res.render('index',{
+//        title : 'Inventory Form',
+//    });
+//    console.log('user accessing Home page');
+//});
+
 app.get('/users', user.list);
+
+app.get('/inventory', inventory.list);
 
 app.post('/inventory/add',function(req,res){
     var inventory = {    
         name : req.body.invName,
-        type : req.body.invType
+        type : req.body.invType,
+        count : req.body.invCount,
+        area : req.body.invArea,
+        shelfNum : req.body.invShelfNum
     };
     
     console.log(inventory);
